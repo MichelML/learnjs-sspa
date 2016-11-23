ef8162259689a577a5b8fe8826dab7e72abe65cc
MichelML <michmoreau.l@gmail.com>
2016-11-22 20:21:14 -0500

    feat: edit markup for proper testing

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -1,6 +1,6 @@
-describe('LearnJS', function() {
-  it('can show a problem view', function() {
+describe("LearnJS", function() {
+  it("can show a problem view", function() {
     learnjs.showView("#problem-1");
-    expect($('.view_container .problem-view').length).toEqual(1);
+    expect($(".view_container .problem-view").length).toEqual(1);
   });
-})
+});
