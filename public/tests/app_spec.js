0d8bb278e13beccbbf3d7ead14d2ce02e303cf45
MichelML <michmoreau.l@gmail.com>
2016-11-26 12:49:13 -0500

    test: add two tests for answer section

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -34,4 +34,19 @@ describe("LearnJS", function() {
         expect(view.find('.title').text()).toEqual('Problem #1');
       });
     });
+
+  describe('answer section', function () {
+    it('can check a correct answer by hitting a button', function() {
+      view.find('.answer').val('true');
+      view.find('.check-btn').click();
+      expect(view.find('.result').text()).toEqual('Correct!');
+    });
+
+    it('can check an incorrect answer', function() {
+      view.find('.answer').val('false');
+      view.find('.check-btn').click();
+      expect(view.find('.result').text()).toEqual('Incorrect!');
+    });
+  
+  })
   });
