1cb0cad8df8a36bd37f9a4d2b3637e35448d1ace
MichelML <michmoreau.l@gmail.com>
2016-11-26 21:17:49 -0500

    feat: add methods for js problems retrieval and answer storage

--- public/app.js
+++ public/app.js
@@ -18,9 +18,9 @@ learnjs.problemView = function(data) {
   var view = $('.templates .problem-view').clone();
   var problemData = learnjs.problems[problemNumber - 1];
   var resultFlash = view.find('.result');
+  var answer = view.find('.answer').val() || 0;
 
   function checkAnswer() {
-    var answer = view.find('.answer').val() || 0;
     var test = problemData.code.replace('__', answer) + '; problem();';
     return eval(test);
   }
@@ -29,6 +29,7 @@ learnjs.problemView = function(data) {
     if (checkAnswer()) {
       var flashElement = learnjs.buildCorrectFlash(problemNumber);
       learnjs.flashElement(resultFlash, flashElement);
+      learnjs.saveAnswer(problemNumber, answer.val());
     } else {
       learnjs.flashElement(resultFlash, 'Incorrect!');
     }
@@ -48,6 +49,13 @@ learnjs.problemView = function(data) {
       buttonItem.remove();
     });
   }
+
+  learnjs.fetchAnswer(problemNumber).then(function(data) {
+    if (data.item) {
+      answer.val(data.Item.answer);
+    }
+  });
+
   return view;
 };
 
@@ -168,4 +176,61 @@ function googleSignIn(googleUser) {
       refresh: refresh
     });
   });
+}
+
+// Database utilities
+learnjs.sendDbRequest = function(req, retry) {
+  var promise = new $.Deferred();
+  req.on('error', function(error) {
+    if (error.code === 'CredentialsError') {
+      learnjs.identity.then(function(identity) {
+        return identity.refresh().then(function() {
+          return retry();
+        }, function() {
+          promise.reject(resp);
+        });
+      });
+    } else {
+      promise.reject(error);
+    }
+  });
+  req.on('success', function(resp) {
+    promise.resolve(resp.data);
+  });
+  req.send();
+  return promise;
+};
+
+learnjs.saveAnswer = function(problemId, answer) {
+  return learnjs.identity.then(function(identity) {
+    var db = new AWS.DynamoDB.DocumentClient();
+    var item = {
+      TableName: 'learnjs',
+      Item: {
+        userId: identity.id,
+        problemId: problemId,
+        answer: answer
+      }
+    };
+    return learnjs.sendDbRequest(db.put(item), function() {
+      return learnjs.saveAnswer(problemId, answer);
+    });
+  });
+};
+
+learnjs.fetchAnswer = function(problemId) {
+  return learnjs.identity.then(function(identity) {
+    var db = new AWS.DynamoDB.DocumentClient();
+    var item = {
+      TableName: 'learnjs',
+      Item: {
+        userId: identity.id,
+        problemId: problemId
+      }
+    };
+    return learnjs.sendDbRequest(db.get(item), function() {
+      return learnjs.fetchAnswer(problemId);
+    });
+  });
+
 }
